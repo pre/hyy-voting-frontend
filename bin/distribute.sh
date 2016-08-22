ce5e9b1bccb4cd6f8d7ddb8a5c3e985f0fd5a668
Petrus Repo <petrus.repo@iki.fi>
2016-08-22 09:47:42 +0300

    chore (scripts) Automate git submodule distribution

---
+++ bin/distribute.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+set -e
+
+# cwd dist
+echo 'Checking out "master" in "dist/"'
+echo 'If this fails do "cd dist && git reset --hard origin/master"'
+echo ''
+pushd dist
+git checkout master
+git clean -dfx
+popd
+
+# cwd /
+grunt build
+pushd dist
+
+echo ""
+git status
+
+echo ""
+echo "All good? Ready for git add? (Y/n)"
+read okay
+if [ "$okay" == "n" ] || [ "$okay" == "N" ]; then
+  exit 0
+fi
+
+git add -A .
+echo "What's the commit message? (eg. theme for the deploy)"
+read message
+git commit -m "$message"
+
+echo "Pushing changes"
+git push
+
+echo ""
+echo "Next: Update the git submodule in API/public folder."
